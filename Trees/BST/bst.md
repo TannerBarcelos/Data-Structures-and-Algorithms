0a7918eb6cec7ef044c08c68bb3b9eb944532499
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-30 19:13:39 -0800

    BFS

--- Trees/BST/bst.md
+++ Trees/BST/bst.md
@@ -19,7 +19,7 @@
 - Like binary search, since the tree is ordered with the same properties, searchng insertion and deletion actually also have the same time complexity as the divide and conquer approach workss on a sorted collection
 - Each item we compare / process will cut the tree search space in half since we have a strict ordering paradigm in the BST. This 'cut-in-half' at a lower level is the mathematical defintion of Logarithms which is why the time complexity is LogN where the number of comparisons to get some piece of data is roughly Log of N where N is the amount of nodes in the tree (e.g Log (8) === 3 <= 3 comparisons)
 
-### The 3 powerful DFS algorithms
+### The 3 powerful DFS algorithms (Depth first - drill down into the tree)
 
 - Can be used for printing, returning, comparing to a search key, etc.
 - See code in the index.js file
@@ -64,6 +64,60 @@
    * else, do nothing
    *
    */
+```
+
+### Breadth first search
+
+![bfs](../img/BFS.png)
+
+- Unlike DFS, we traverse across the breadth (width) of the tree horizontally.
+- This algorithm is very important
+
+```text
+Algorithm
+
+Check first if the tree is empty and return if so
+
+Step 1)
+  Establish a queue and a results array for the trees node values in Breadth First order
+  let res = []
+  let queue = []
+
+Step 2)
+  Push the root of the tree onto the queue
+
+Step 3)
+  While the queue is not empty
+    - Pop off the element at the front and put it in the results array
+    - Check if this node has a left and right child and if they do
+      - push the left child into the Queue
+      - push this right child into the queue
+    - If it does not, we reached the last node and the queue will be empty and looping is done
+```
+
+##### Code
+
+```javascript
+bfs = () => {
+  if (!this.root) return;
+
+  const result = [];
+  const queue = [];
+  queue.push(this.root);
+
+  while (queue.length) {
+    let currentNode = queue.shift();
+    result.push(currentNode.data);
+
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  return result;
+};
 ```
 
 <br>
--- Trees/BST/index.js
+++ Trees/BST/index.js
@@ -13,65 +13,124 @@ class BST {
 
   // We use public methods to then call private methods such that we can access
   // the root of the tree while not exposing it to the outside world: this is called 'encapsulation' or 'data hiding'
-  insert(data) {
+  insert = (data) => {
     this.size++;
-    // If tree is empty, insert
     if (!this.root) {
       this.root = new Node(data);
     } else {
-      // else use the root and a helper method to get the insertion point
-      this._insert(this.root, data);
+      const _insert = (node, data) => {
+        if (node.data > data) {
+          if (!node.left) {
+            node.left = new Node(data);
+            return;
+          }
+          _insert(node.left, data);
+        }
+        if (node.data < data) {
+          if (!node.right) {
+            node.right = new Node(data);
+            return;
+          }
+          _insert(node.right, data);
+        }
+      };
+      // Call the insert method to insert
+      _insert(this.root, data);
     }
-  }
+  };
 
-  _insert(node, data) {
-    if (node.data > data) {
-      if (!node.left) {
-        node.left = new Node(data);
-        return;
+  /**
+   * The following traversals use a nested function because we cannot pass an array to a recursive function
+   * and retain the contents of it across recrusive calls. We nest like this to allow the array to be block scoped and global to the nested function.
+   * Instead of printing in the ensted function like we know for the traversals, we push to our output array instead
+   */
+
+  // Traverses the tree in inOrder traversal populating an array of that tree inOrder
+  dfsInorder = () => {
+    let arr = [];
+    const _inOrder = (node) => {
+      if (node !== null) {
+        _inOrder(node.left);
+        arr.push(node.data);
+        _inOrder(node.right);
       }
-      this._insert(node.left, data);
-    }
-    if (node.data < data) {
-      if (!node.right) {
-        node.right = new Node(data);
-        return;
+    };
+    _inOrder(this.root, arr);
+    return arr;
+  };
+
+  // Traverses the tree in PreOrder traversal populating an array of that tree in PreOrder
+  dfsPreOrder = () => {
+    const arr = [];
+    const _preOrder = (node) => {
+      if (node !== null) {
+        arr.push(node.data);
+        _preOrder(node.left);
+        _preOrder(node.right);
       }
-      this._insert(node.right, data);
-    }
-  }
+    };
+    _preOrder(this.root);
+    return arr;
+  };
 
-  dfsInorder() {
-    this._inOrder(this.root);
-  }
-  _inOrder(node) {
-    if (node !== null) {
-      this._inOrder(node.left);
-      console.log(node.data);
-      this._inOrder(node.right);
-    }
-  }
-  dfsPreOrder() {
-    this._preOrder(this.root);
-  }
-  _preOrder(node) {
-    if (node !== null) {
-      console.log(node.data);
-      this._inOrder(node.left);
-      this._inOrder(node.right);
-    }
-  }
+  // Traverses the tree in PostOrder traversal populating an array of that tree in PostOrder
+  dfsPostOrder = () => {
+    const arr = [];
+    const _postOrder = (node) => {
+      if (node !== null) {
+        _postOrder(node.left);
+        _postOrder(node.right);
+        arr.push(node.data);
+      }
+    };
+    _postOrder(this.root);
+    return arr;
+  };
 
-  dfsPostOrder() {
-    this._postOrder(this.root);
-  }
-  _postOrder(node) {
-    if (node !== null) {
-      this._inOrder(node.left);
-      this._inOrder(node.right);
-      console.log(node.data);
+  bfs = () => {
+    if (!this.root) return;
+
+    const result = [];
+    const queue = [];
+    queue.push(this.root);
+    // While the queue is not empty
+    while (queue.length) {
+      // Get the front of the queue and remove it and append it to our result array
+      let currentNode = queue.shift();
+      result.push(currentNode.data);
+
+      // Check if this node has children. If it does, add them to the queue and then continue iteration
+      // Make sure you check left then right or vice versa if you want to do reverse
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
     }
-  }
+
+    // Return the result
+    return result;
+  };
+
+  // Uses a nested find function with this.root to search left and right tree for the data
+  find = (data) => {
+    if (!this.root || !data) return false;
+    const _find = (node, data) => {
+      if (!node) {
+        return false;
+      } else if (data < node.data) {
+        return _find(node.left, data);
+      } else if (data > node.data) {
+        return _find(node.right, data);
+      } else {
+        return true;
+      }
+    };
+    return _find(this.root, data);
+  };
+
+  getSize = () => this.size;
 }
 
 const tree = new BST();
@@ -81,8 +140,7 @@ tree.insert(101);
 tree.insert(8);
 tree.insert(11);
 
-tree.dfsInorder();
-console.log('\n\n');
-tree.dfsPreOrder();
-console.log('\n\n');
-tree.dfsPostOrder();
+console.log(tree.dfsInorder());
+console.log(tree.dfsPreOrder());
+console.log(tree.dfsPostOrder());
+console.log(tree.bfs());
